572e386c5489f4f8468c57a5d3e5b7a8125be537
Adan Malik <malikbruh102@gmail.com>
2025-01-12 04:25:16 +0500

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { navItems } from "@/data";
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import { FloatingNav } from "@/components/FloatingNavbar";
+import RecentProjects from "@/components/RecentProjects";
+import Clients from "@/components/Clients";
+import Footer from "@/components/Footer";
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero/>
+        <Grid/>
+        <RecentProjects/>
+        <Clients/>
+        <Footer/>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ data/index.ts
@@ -0,0 +1,186 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "We prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "Tech Stacks",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiasts creating impactful solutions",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Building innovative tools that shape the future of technology.",
+      description: "Innovating Future",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "BrevityAI - AI-Assisted Resume Generator",
+      des: "Effortlessly craft professional resumes with AI-powered precision and creativity.",
+      img: "/p1.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg","/clerk.png","gemini.png"],
+      link: "https://brevity-ai.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "SmartCardAI - AI FlashCard Generator",
+      des: "Create intelligent flashcards instantly with the power of AI for smarter learning.",
+      img: "/p2.png",
+      iconLists: ["/node.png", "re.svg","/tail.svg", "/ts.svg", "/gemini.png"],
+      link: "https://smart-card-ai.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Ambient Lights - A Flutter Application",
+      des: "Enhance your surroundings with dynamic lighting control through this sleek Flutter app.",
+      img: "/p3.png",
+      iconLists: ["/flutter.svg"],
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Interactive Alzheimer Detection Application",
+      des: "An intuitive app leveraging advanced AI to assist in early Alzheimer’s detection with interactive features.",
+      img: "/p2.png",
+      iconLists: ["/flutter.svg",],
+      link: "",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with EigenStudios was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. EigenStudios's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, EigenStudios is the ideal partner.",
+      name: "Adan Malik",
+      title: "Co-Founder of EigenStudios",
+    },
+    {
+      quote:
+        "Collaborating with EigenStudios was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. EigenStudios's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, EigenStudios is the ideal partner.",
+      name: "Adan Malik",
+      title: "Co-Founder of EigenStudios",
+    },
+    {
+      quote:
+        "Collaborating with EigenStudios was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. EigenStudios's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, EigenStudios is the ideal partner.",
+      name: "Adan Malik",
+      title: "Co-Founder of EigenStudios",
+    },
+    {
+      quote:
+        "Collaborating with EigenStudios was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. EigenStudios's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, EigenStudios is the ideal partner.",
+      name: "Adan Malik",
+      title: "Co-Founder of EigenStudios",
+    },
+    {
+      quote:
+        "Collaborating with EigenStudios was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. EigenStudios's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, EigenStudios is the ideal partner.",
+      name: "Adan Malik",
+      title: "Co-Founder of EigenStudios",
+    },
+  ];
+  
+  /*export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];*/
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
